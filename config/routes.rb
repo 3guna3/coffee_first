d3f57653570ce4a37591cb4d0653ccb86abe53f7
kohei <shoot0321@gmail.com>
2021-09-01 09:04:21 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get "beans/create"
-  get "beans/show"
-  get "beans/index"
-  get "shops/create"
-  get "shops/show"
-  get "shops/index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "shops#index"
+  resources :shops, only: [:create, :show, :index]
+  resources :beans, only: [:create, :show, :index]
 end
