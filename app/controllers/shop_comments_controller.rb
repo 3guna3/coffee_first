c49175724960edb0f556a062273c9b27e218e8f1
kohei <shoot0321@gmail.com>
2021-09-03 09:21:56 +0900

    shop_commentsコントローラを作成しshow以外のアクションをログイン必須に変更

--- app/controllers/shop_comments_controller.rb
+++ app/controllers/shop_comments_controller.rb
@@ -1,2 +1,12 @@
 class ShopCommentsController < ApplicationController
+  before_action :authenticate_user, { only: [:create, :edit, :update, :destroy] }
+  def create; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  def show; end
 end
