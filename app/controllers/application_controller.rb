94cafb3bd4d596ceb758f2b47094b8ca0eb059ce
kohei <shoot0321@gmail.com>
2021-09-08 14:39:19 +0900

    アカウント登録ページを編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :img])
     devise_parameter_sanitizer.permit(:acount_update, keys: [:name, :self_introduction, :img])
   end
 end
