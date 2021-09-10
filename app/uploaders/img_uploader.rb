e0d7563c2d26b37cdf24d111b7b5a42001d626e3
kohei <shoot0321@gmail.com>
2021-09-10 13:05:19 +0900

    画像のファイル形式を変更

--- app/uploaders/img_uploader.rb
+++ app/uploaders/img_uploader.rb
@@ -27,11 +27,16 @@ class ImgUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+  process convert: "jpg"
+
+  def filename
+    super.chomp(File.extname(super)) + ".jpg" if original_filename.present?
+  end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [100, 100]
+  end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
