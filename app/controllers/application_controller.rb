30cef0272da96dda85550da5fe330b5e75b95550
kohei <shoot0321@gmail.com>
2021-09-10 10:10:22 +0900

    ユーザー詳細ページのレイアウトを編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :img])
-    devise_parameter_sanitizer.permit(:acount_update, keys: [:name, :self_introduction, :img])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, :img])
   end
 end
