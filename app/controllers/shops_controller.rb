4acace8068ca081125a6b090aa04877bb50796a4
kohei <shoot0321@gmail.com>
2021-09-13 11:27:32 +0900

    shopコントローラのcreateアクションを設定

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,11 +5,20 @@ class ShopsController < ApplicationController
     @shop = Shop.new
   end
 
-  def create; end
+  def create
+    Shop.create!(name: shop_params[:name], prefecture: shop_params[:prefecture], address: shop_params[:address], TEL: shop_params[:TEL], HP: shop_params[:HP],
+                 img: shop_params[:img], user_id: current_user.id)
+  end
 
   def show; end
 
   def index
     @shops = Shop.all
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :prefecture, :address, :TEL, :HP, :img)
   end
 end
