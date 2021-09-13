5d91bfb1a0cb18ff01b35607938e868a0a10f9ec
kohei <shoot0321@gmail.com>
2021-09-13 09:48:01 +0900

    shopコントローラにnewを追記

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,10 @@
 class ShopsController < ApplicationController
   before_action :authenticate_user, { only: [:create] }
 
+  def new
+    @shop = Shop.new
+  end
+
   def create; end
 
   def show; end
