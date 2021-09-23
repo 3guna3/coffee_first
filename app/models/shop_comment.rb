043fd3d3107f86a7eaeea7b2d0f59f2efed4d45f
kohei <shoot0321@gmail.com>
2021-09-23 11:53:10 +0900

    店舗一覧の星評価の不具合を修正

--- app/models/shop_comment.rb
+++ app/models/shop_comment.rb
@@ -2,8 +2,9 @@ class ShopComment < ApplicationRecord
   belongs_to :user
   belongs_to :shop
 
-  with_options presence: true do
-    validates :content
-    validates :rate
-  end
+  validates :content, presence: true
+  validates :rate, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  }, presence: true
 end
