9ddfd6da14572c562b7002ed98abdd9da8a387ee
kohei <shoot0321@gmail.com>
2021-09-28 17:05:32 +0900

    コーヒー豆の検索フォームを編集

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -1,7 +1,7 @@
 class BeansController < ApplicationController
   before_action :authenticate_user!, { except: [:index, :show] }
   before_action :set_bean, { only: [:show, :edit, :update, :destroy] }
-  before_action :set_bean_q, { only: [:index] }
+  before_action :set_bean_q, { only: [:index, :search] }
 
   def index
     @beans = Bean.includes(:user).order(:created_at)
