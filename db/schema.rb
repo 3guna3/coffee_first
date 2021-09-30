df83fe015b724418b84a3274a5f709f90e36f55d
kohei <shoot0321@gmail.com>
2021-09-30 09:12:35 +0900

    Shopsテーブルにaverage_rateカラムを追加しdb:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_013537) do
+ActiveRecord::Schema.define(version: 2021_09_30_001116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_013537) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.float "rate_average", default: 0.0
     t.index ["user_id"], name: "index_shops_on_user_id"
   end
 
