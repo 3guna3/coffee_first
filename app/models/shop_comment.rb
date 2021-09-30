45eef95172f24206f43212134aa5bb3742c90b5a
kohei <shoot0321@gmail.com>
2021-09-30 09:23:09 +0900

    shop_comment.rbにShopモデルのrate_averageカラムが更新されるような処理を記載

--- app/models/shop_comment.rb
+++ app/models/shop_comment.rb
@@ -4,4 +4,9 @@ class ShopComment < ApplicationRecord
 
   validates :content, presence: true
   validates :rate, presence: true
+
+  after_save :update_rate_average
+  after_delete :update_rate_average
+
+  delegate :update_rate_average, to: :shop
 end
