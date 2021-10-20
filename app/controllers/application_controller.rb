32218471497ab86e774111bcafc874c24dda4901
kohei <shoot0321@gmail.com>
2021-10-20 16:14:27 +0900

    管理者画面でログインした後に通常のログイン先に遷移してしまう不具合を解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    beans_path(resource)
+    if resource.is_a?(AdminUser)
+      admin_root_path
+    else
+      beans_path(resource)
+    end
   end
 
   private
