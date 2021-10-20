a74b5c556f0be3b3689e9063ec8850767dbfc9b6
kohei <shoot0321@gmail.com>
2021-10-20 13:22:50 +0900

    app/admin/beans.rbのカラム・フォームを修正

--- app/admin/beans.rb
+++ app/admin/beans.rb
@@ -1,18 +1,51 @@
 ActiveAdmin.register Bean do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :price, :country, :farm, :variety, :screen_size, :img, :user_id, :shop_id, :rate_average
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :price, :country, :farm, :variety, :screen_size, :img, :user_id, :shop_id, :rate_average]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-
   permit_params :name, :price, :country, :farm, :variety, :screen_size, :img, :user_id, :shop_id
+
+  index do
+    selectable_column
+
+    id_column
+    column :name
+    column :price
+    column :country
+    column "登録者" do |bean|
+      bean.user.name
+    end
+    column "販売店" do |bean|
+      bean.shop.name
+    end
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :name
+  filter :price
+  filter :country
+  filter :created_at
+
+  show do |bean|
+    attributes_table(*bean.class.columns.collect { |column| column.name.to_sym })
+    panel "コメント一覧" do
+      table_for bean.beans_comments do
+        column :user
+        column :content
+        column :created_at
+      end
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :price
+      f.input :country
+      f.input :farm
+      f.input :variety
+      f.input :screen_size
+      f.input :img
+    end
+    f.actions
+  end
 end
