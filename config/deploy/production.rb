00916c89da19d307f2a07627411f1e51055d35c5
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-10-28 19:39:27 +0900

    config/deploy.rbにmaster以外のブランチを反映させる処理を追記

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,6 +6,8 @@ set :ssh_options, {
   auth_methods: %w[publickey]
 }
 
+set :branch, ENV["BRANCH"] || "master"
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
